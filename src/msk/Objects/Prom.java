dcda7a0f06bbddf773528c94a40b062dd6c0eb7a
Szymon Sobotkiewicz <sz.sobotkiewicz@gmail.com>
2018-09-19 09:40:32 +0200

    dodanie ambasadorów i federatów

---
+++ src/msk/Objects/Prom.java
@@ -0,0 +1,24 @@
+package msk.Objects;
+
+import msk.BaseObject;
+
+public class Prom extends BaseObject {
+        private int liczbaWolnychMiejsc;
+        private int numerStacji;
+
+    public int getLiczbaWolnychMiejsc() {
+        return liczbaWolnychMiejsc;
+    }
+
+    public void setLiczbaWolnychMiejsc(int liczbaWolnychMiejsc) {
+        this.liczbaWolnychMiejsc = liczbaWolnychMiejsc;
+    }
+
+    public int getNumerStacji() {
+        return numerStacji;
+    }
+
+    public void setNumerStacji(int numerStacji) {
+        this.numerStacji = numerStacji;
+    }
+}
